a768e91ce6a5f95202f9003754c65206233ffe41
Aziangelo <aziangelob@gmail.com>
2024-06-02 12:09:10 +0800

    Win Material Test

--- include/azify/utils/functions.glsl
+++ include/azify/utils/functions.glsl
@@ -48,6 +48,7 @@ float noise(vec2 x) {
 }
 
 // Define custom fract function if it's not available
+/*
 vec2 customFract(vec2 x) {
     return x - floor(x);
 }
@@ -57,7 +58,7 @@ float voronoi(vec2 pos, float time) {
     mat2 m = mat2(7, -5, 5, 7) * 0.1;
     
     return min(length(fract(p + tt) - vec2(0.5,0.5)),length(fract((p + vec2(0.5,0.5) - tt * 0.2) * m) - vec2(0.5,0.5)));
-}
+}*/
 
 // SKY FUNCTION
 vec3 dynamicSky(vec3 diff, vec3 skyPos, float isNight, float isDusk, float isRain, vec3 skycolor, vec3 fogcolor) {
@@ -111,10 +112,10 @@ vec3 dynamicSky(vec3 diff, vec3 skyPos, float isNight, float isDusk, float isRai
     diff = color;
     return diff;
 }
-
+/*
 float sunDirShadow(vec4 color0, vec2 lightmapUV) {
      float shadow = smoothstep(0.885, 0.71, color0.y + 0.2 * (color0.y - color0.z));
     return mix(shadow, 0.0, pow(lightmapUV.x, 3.0));
-}
+}*/
 
 #endif
