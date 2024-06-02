8ec5348aedc7e1b2bb38cf2f096774e716f11b70
Aziangelo <aziangelob@gmail.com>
2024-06-02 10:54:43 +0800

    Win Material Test

--- include/azify/utils/functions.glsl
+++ include/azify/utils/functions.glsl
@@ -61,7 +61,7 @@ float voronoi(vec2 pos, float time) {
         length(fract((p + vec2(0.5) - tt * 0.2) * m) - vec2(0.5))
     );
 }
-
+/*
 // SKY FUNCTION
 vec3 dynamicSky(vec3 diff, vec3 skyPos, float isNight, float isDusk, float isRain, vec3 skycolor, vec3 fogcolor) {
     // Precomputed constant Colors
@@ -111,7 +111,7 @@ vec3 dynamicSky(vec3 diff, vec3 skyPos, float isNight, float isDusk, float isRai
 
     diff = color;
     return diff;
-}
+}*/
 
 float sunDirShadow(vec4 color0, vec2 lightmapUV) {
      float shadow = smoothstep(0.885, 0.71, color0.y + 0.2 * (color0.y - color0.z));
